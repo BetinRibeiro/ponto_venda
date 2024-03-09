99f834dd1cf0b8d8b8104610bc033269e9c4cf56
Rogoberto Ribeiro <usbetin@gmail.com>
2024-03-09 01:12:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const cryptocurrencies = [
     ["ADA","1.6","1020"],
     ["NEAR","2.16","187.3"],
     ["SOL","64.7","0.881"],
-    ["AGIX","0.27","611.67"]
+    ["AGIX","0.27","611.67"],
+    ["AGIX","0.27","200"],
+    ["ETH","2850","0.0162"]
 ];
+
+// ["SDAO","0.6522","22.84922"]
 
 async function fetchCryptoPrice(crypto) {
     const response = await fetch(`https://api.binance.com/api/v3/ticker/price?symbol=${crypto}USDT`);
@@ -20,6 +24,22 @@ function real(value) {
         currency: 'BRL'
     }).format(value*5);
 }
+
+function calcularPercentual(valor1, valor2) {
+    // Verifica se os valores são válidos e não são zero para evitar divisão por zero
+    if (isNaN(valor1) || isNaN(valor2) || valor1 === 0 || valor2 === 0) {
+        return "Valores inválidos";
+    }
+    
+    // Calcula o percentual
+    const percentual = ((valor2 - valor1) / valor1) * 100;
+
+    // Formata o percentual com duas casas decimais e o símbolo de percentagem (%)
+    const percentualFormatado = percentual.toFixed(2) + "%";
+
+    return percentualFormatado;
+}
+
 async function updateTable() {
     const tableBody = document.querySelector('#cryptoTable tbody');
     tableBody.innerHTML = '';
@@ -42,7 +62,8 @@ async function updateTable() {
                         <td>${dolar(total_compra)}</td>
                         <td>${dolar(preco_atual)}</td>
                         <td>${dolar(totalAtualizado)}</td>
-                        <td>${dolar(lucro)}</td>
+                        <td>${dolar(lucro)} </td>
+                        <td>${calcularPercentual(total_compra,totalAtualizado) }</td>
                     </tr>`;
         tableBody.innerHTML += row;
     }
@@ -55,6 +76,7 @@ async function updateTable() {
                         <td>Total</td>
                         <td>${dolar(total_atualizado_geral)}</td>
                         <td>${dolar(total_atualizado_geral - total_compra_geral)}</td>
+                        <td>${calcularPercentual(total_compra_geral,total_atualizado_geral ) }</td>
                     </tr>
                     <tr class="table-secondary">
                     <td colspan="3">Total em Real</td>
@@ -62,6 +84,7 @@ async function updateTable() {
                         <td>Total</td>
                         <td>${real(total_atualizado_geral)}</td>
                         <td>${real(total_atualizado_geral - total_compra_geral)}</td>
+                        <td>${calcularPercentual(total_compra_geral,total_atualizado_geral ) }</td>
                     </tr>`;
     tableFoot.innerHTML += row;
 }
